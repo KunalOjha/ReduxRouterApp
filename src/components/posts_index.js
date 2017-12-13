513cfedfd954dd3fc0c0177a0a425140ec0c2576
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2017-12-13 01:52:41 -0500

    adding in redux- initial AC for PostsIndex component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
+import {fetchPosts} from '../actions';
+import {connect} from 'react-redux';
 
-export default class PostsIndex extends Component {
-    render(){
+class PostsIndex extends Component {
+   
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
+    render() {
         return (
             <div>Posts Index</div>
         )
     }
 }
+
+//shortcut for writing matchStateToProps ;)
+export default connect(null, {fetchPosts})(PostsIndex);
