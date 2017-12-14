92a926684e18af41e0be05641ee6b58415a53366
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2017-12-14 02:05:45 -0500

    redux forms error validation added, not yet fully functional

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,63 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form'
 
 class PostsNew extends Component {
+    
+    renderTitleField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+    
     render() {
         return (
-            <div>Posts New </div>
+            <form>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderTitleField}
+                />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderTitleField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderTitleField}
+                />
+             </form>
         )
     }
 }
 
-export default PostsNew;
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        values.title ="Enter a title"
+    };
+
+    if (!values.categories) {
+        values.title ="Enter some categories"
+    };
+
+    if (!values.content) {
+        values.title ="Enter some content"
+    };
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
